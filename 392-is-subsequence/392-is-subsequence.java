e0139c89e0f4a0022d78493d32a3d6389756feac
Anteneh Admasu <39303685+AntenehAdmasu@users.noreply.github.com>
2022-03-14 14:33:29 +0300

    Time: 1 ms (89.07%), Space: 42.8 MB (9.46%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int len = s.length();
+        if(len == 0) return true;
+        int sptr = 0;
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            if(sptr < len && c == s.charAt(sptr)) sptr++;
+            if(sptr == len) return true;
+        }
+        
+        return false;
+    }
+    
+}

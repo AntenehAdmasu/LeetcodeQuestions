943aa7a239b90a217cab7a6785f1622f8aa73b09
Anteneh Admasu <39303685+AntenehAdmasu@users.noreply.github.com>
2022-03-31 21:39:45 +0300

    Time: 265 ms (32.34%), Space: 73.2 MB (7.27%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -1,20 +1,7 @@
 class Solution {
     public int maximumDetonation(int[][] bombs) {
         int size = bombs.length;
-        // [0] -> 1,2
-        // [1] ->
-        // [2] -> 1,3
-        // [3] -> 1,2,4
-        // [4] -> 3
-        
-        // Construct the list
-        
-        // Traverse from every node(bomb) 
-        
-        //Count DFS
-        
-        // If every node is seen(remaining < maximum) - return
-        HashMap<Integer, Set<Integer>> adjacencyList = new HashMap<>();
+        HashMap<Integer, List<Integer>> adjacencyList = new HashMap<>();
         
         for(int i = 0; i < size; i++){
             for (int j = 0; j < size; j++){
@@ -24,11 +11,10 @@ class Solution {
                 // Calculate the distance
                 boolean inRange = withInRange(bombs, i, j);
                 if(inRange){
-                    Set<Integer> neighbours = adjacencyList.getOrDefault(i, new HashSet<>());
+                    List<Integer> neighbours = adjacencyList.getOrDefault(i, new ArrayList<>());
                     neighbours.add(j);
                     adjacencyList.put(i, neighbours);
                 }
-                
             }
         }
         
@@ -46,15 +32,14 @@ class Solution {
     }
     
     
-    private int dfs(int index, HashSet<Integer> visited, HashMap<Integer, Set<Integer>> adjacencyList){
+    private int dfs(int index, HashSet<Integer> visited, HashMap<Integer, List<Integer>> adjacencyList){
         int count = 1;
-        Set<Integer> neighbours = adjacencyList.get(index);
+        List<Integer> neighbours = adjacencyList.get(index);
         if(neighbours != null){
             
             Iterator<Integer> it = neighbours.iterator();
 
-            while(it.hasNext()){
-                int neighbourIndex = it.next();
+            for(int neighbourIndex: neighbours){
                 if(!visited.contains(neighbourIndex)){
                     visited.add(neighbourIndex);
                     count += dfs(neighbourIndex, visited, adjacencyList);

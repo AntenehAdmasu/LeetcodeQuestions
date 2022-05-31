4b32e441e02e3e92465aad9ca6acde1b658ad15f
Anteneh Admasu <39303685+AntenehAdmasu@users.noreply.github.com>
2022-05-31 10:17:09 +0300

    Time: 584 ms (5.21%), Space: 125.5 MB (32.81%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        int len = s.length();
+        int lowerLimit = 0;
+        int upperLimit = (int) Math.pow(2, k) - 1;
+        Set<Integer> found = new HashSet<>();
+        
+        for(int i = 0; i <= len - k; i++){
+            String binaryString = s.substring(i, i + k);
+            int decimal = Integer.parseInt(binaryString,2);
+            if(decimal <= upperLimit && decimal >= lowerLimit){
+                found.add(decimal);
+            }
+        }
+        return found.size() == upperLimit - lowerLimit + 1;
+    }
+}

c92437853dba417efb7a434c974a1b65847c9f16
Anteneh Admasu <39303685+AntenehAdmasu@users.noreply.github.com>
2022-06-28 21:57:47 +0300

    Time: 15 ms (69.91%), Space: 54.7 MB (40.36%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,23 +1,24 @@
 class Solution {
     public int minDeletions(String s) {
-        int[] freq = new int[26];
         int deletions = 0;
+        int[] freq = new int[26];
         for(char c: s.toCharArray()){
             freq[c - 'a']++;
         }
+        
         Set<Integer> usedFreq = new HashSet<>();
         for(int i: freq){
             if(i != 0){
                 boolean added = usedFreq.add(i);
+                // If frequency is repeated
                 if(!added){
-                    // Find prev spot not taken
-                    int next = i;
-                    while(usedFreq.contains(next)){
-                        next--;
+                    // Find previous spot not taken or zero
+                    int nextEmpty = i - 1;
+                    while(usedFreq.contains(nextEmpty) && nextEmpty != 0){
+                        nextEmpty--;
                     }
-                    deletions += i - next;
-                    if(next != 0)
-                        usedFreq.add(next);
+                    deletions += i - nextEmpty;
+                    usedFreq.add(nextEmpty);
                 }
             }
         }

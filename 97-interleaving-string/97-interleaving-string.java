1078f4436034674f59cd73af771ac035788b04cc
Anteneh Admasu <39303685+AntenehAdmasu@users.noreply.github.com>
2022-07-07 14:07:57 +0300

    Time: 19 ms (11.46%), Space: 45.2 MB (10.96%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,29 +1,30 @@
-// Non dp solution
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         return constructS3(s1, s2, s3, 0, 0, 0, new HashMap<String, Boolean>());
     }
     
-    private boolean constructS3(String s1, String s2, String s3, int pos1, int pos2, int curr, HashMap<String, Boolean> dp){
-        String key = pos1 + " " + pos2 + " " + " " + curr;
+    private boolean constructS3(String s1, String s2, String s3, int p1, int p2, int curr, HashMap<String, Boolean> dp){
+        String key = p1 + "|" + p2 + "|" + "|" + curr;
         if(dp.containsKey(key)){
             return dp.get(key);
         }
-        // If s3 is constructed with all elements from s1 and s2 consumed
-        if(curr == s3.length() && pos1 == s1.length() && pos2 == s2.length()){
+        // If s3 is constructed with all characters from s1 and s2
+        if(curr == s3.length() && p1 == s1.length() && p2 == s2.length()){
             return true;
         }
         // If s3 is constructed but there are characters from s1 or s2 not used
         if(curr >= s3.length()){
             return false;
         }
+        char currentChar = s3.charAt(curr);
         boolean works = false;
-        if(pos1 < s1.length() && s1.charAt(pos1) == s3.charAt(curr)){
-            works |= constructS3(s1, s2, s3, pos1+1, pos2, curr+1, dp);
+        if(p1 < s1.length() && s1.charAt(p1) == currentChar){
+            works |= constructS3(s1, s2, s3, p1+1, p2, curr+1, dp);
         }
-        if(pos2 < s2.length() && s2.charAt(pos2) == s3.charAt(curr)){
-            works |= constructS3(s1, s2, s3, pos1, pos2+1, curr+1, dp);
+        if(p2 < s2.length() && s2.charAt(p2) == currentChar){
+            works |= constructS3(s1, s2, s3, p1, p2+1, curr+1, dp);
         }
+
         dp.put(key, works);
         return works;        
     }

14c48aa9c94de3d09dbb4093532fd99d426a7b87
Anteneh Admasu <39303685+AntenehAdmasu@users.noreply.github.com>
2022-07-28 14:47:30 +0300

    Time: 24 ms (21.34%), Space: 46.3 MB (23.74%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> smap = new HashMap<>();
+        HashMap<Character, Integer> tmap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(smap.get(c) == null){
+                smap.put(c,1);
+            }else{
+                smap.put(c, smap.get(c)+1);
+            }
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            char c = t.charAt(j);
+            if(tmap.get(c) == null){
+                tmap.put(c,1);
+            }else{
+                tmap.put(c, tmap.get(c)+1);
+            }
+        }
+        
+        if(smap.values().size() != tmap.values().size()) return false;
+
+        for (Character c: smap.keySet() ) {
+            if(!(smap.get(c).equals((tmap.get(c))))) return false;
+        }
+        return true;
+    }
+}
